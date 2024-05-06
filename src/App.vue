9760c550f74837e6b8765dcf0f4dcf4f6a6576da
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-06 20:01:46 +0200

    feat: fix to project components and cards

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <script>
 import axios from 'axios';
-import AppCard from './components/AppCard.vue';
 
+import AppCards from './components/AppCards.vue';
 export default {
   components: {
-    AppCard
+    
+    AppCards
   },
   data() {
     return {
-      character: [],
+      characters: [],
     }
   },
   created() {
     axios
     .get("https://rickandmortyapi.com/api/character")
     .then((resp) => {
-      this.character = resp.data.results;
-      console.log(this.character);
+      this.characters = resp.data.results;
+      console.log(this.characters);
     })
   }
 }
 </script>
 
 <template>
-  <h1 class="text-center py-4">Rick and Morty App</h1>
+  <h1 class="text-center py-5">Rick and Morty App</h1>
 
-  <AppCard />
+  <AppCards :characters="characters"/>
 </template>
 
-<style scoped>
-
+<style lang="scss">
 </style>
