f077048ef696d56d2afbf81a14ece2f09bc0cb6d
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-06 19:18:14 +0200

    feat: project main components

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <script>
 import axios from 'axios';
+import AppCard from './components/AppCard.vue';
+
 export default {
   components: {
+    AppCard
   },
   data() {
     return {
+      character: [],
     }
   },
   created() {
     axios
     .get("https://rickandmortyapi.com/api/character")
     .then((resp) => {
-      console.log(resp);
+      this.character = resp.data.results;
+      console.log(this.character);
     })
   }
 }
 </script>
 
 <template>
- 
+  <h1 class="text-center py-4">Rick and Morty App</h1>
+
+  <AppCard />
 </template>
 
 <style scoped>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,20 @@
+<script>
+import CardsList from './CardsList.vue';
+export default {
+    components: {
+        CardsList
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <CardsList />
+
+</template>
+
+<style scoped lang="scss">
+</style>
